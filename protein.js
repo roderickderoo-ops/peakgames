0c31774f730f8059ed19b20c6f14d4f991e63db8
roderickderoo-ops <roderick.deroo@leerling.molenland.be>
2026-03-19 17:21:20 +0100

    Add files via upload

--- protein.js
+++ protein.js
@@ -7,7 +7,9 @@ let proteinShakerCost = 100;
 let proteinShakers = 0;
 let proteinShakerMult = 1;
 let ricebotCostMult = 1.01;
-let tier1cost;
+let factories = 0;
+let factorieCost = 1500;
+let factorieMult = 8;
 
 let friedChickenBtn = document.getElementById("friedChickenBtn");
 let proteinLabel = document.getElementById("proteinLabel");
@@ -25,7 +27,7 @@ let buildings = [
         effect(){
             riceBots++;
             protein -= ricebotCost;
-            ricebotCost =1 + Math.floor(ricebotCost * 1.1 * ricebotCostMult);
+            ricebotCost =1 + Math.floor(ricebotCost * 1.15);
             ricebotCostMult *= 1.01;
             riceCostLabel.textContent ="🍗" + ricebotCost;
         }
@@ -39,6 +41,16 @@ let buildings = [
             protein -= proteinShakerCost;
             proteinShakerCost = Math.floor(proteinShakerCost * 1.15);
         }
+    },
+    {
+        id: "factories",
+        name: "factories",
+        getCost:() => factorieCost,
+        effect(){
+            factories++;
+            protein -= factorieCost;
+            factorieCost = Math.floor(factorieCost * 1.15);
+        }
     }
 ]
 
@@ -47,6 +59,7 @@ let upgrades = [
         id: "clickTier1",
         name: "doubleClick",
         tier: 1,
+        title: "Your mouse gets twice as strong",
         cost: 100,
         requirement: () => riceBots >= 1,
         bought: false,
@@ -56,8 +69,9 @@ let upgrades = [
     },
     {
         id: "clickTier2",
-        name: "quadrupleClick",
+        name: "ambidextrous",
         tier: 2,
+        title: "Your mouse gets twice as strong again",
         requirement: () => riceBots >= 5,
         cost: 500,
         bought: false,
@@ -69,6 +83,7 @@ let upgrades = [
         id: "clickTier3",
         name: "dr Octofinger",
         tier: 3,
+        title: "Your mouse gets twice as strong again",
         requirement: () => riceBots >= 25,
         cost: 1000,
         bought: false,
@@ -78,8 +93,9 @@ let upgrades = [
     },
     {
         id: "riceTier1",
-        name: "AA battery",
+        name: "Temu batteries",
         tier: 1,
+        title: "Doubles the speed of the ricebots",
         requirement: () => riceBots >= 1,
         cost: 100,
         bought: false,
@@ -89,9 +105,10 @@ let upgrades = [
     },
     {
     id: "riceTier2",
-        name: "AAAA battery",
+        name: "E-bay batteries",
         cost: 500,
         tier: 2,
+        title: "Doubles the speed of the ricebots again",
         requirement: () => riceBots >= 5,
         bought: false,
         effect: function(){
@@ -99,13 +116,59 @@ let upgrades = [
         }
     },
     {
-        id: "2X Protein powder",
+        id: "riceTier3",
+        name: "nuclear batteries",
+        cost: 10000,
+        tier: 3,
+        title: "Triples the speed of the ricebots ",
+        requirement: () => riceBots >=25,
+        bought: false,
+        effect: function(){
+            ricebotMult *= 3;
+        }
+    },
+    {
+        id: "shakerTier1",
         name: "2X Protein powder",
-        cost: 1500,
+        cost: 500,
+        title: "Doubles shaker output",
         requirement: () => proteinShakers >= 1,
         bought: false,
         effect(){
             proteinShakerMult *= 2;
+        }
+    },
+    {
+        id: "shakerTier2",
+        name: "I CAN'T HIT MY PROTEIN",
+        cost: 1000,
+        title: "500 PROTEIN SHAKES (2X output)",
+        requirement: () => proteinShakers >= 5,
+        bought: false,
+        effect(){
+            proteinShakerMult *= 2;
+        }
+    },
+    {
+        id: "shakerTier3",
+        name: "Nuclear protein shakers",
+        cost: 10000,
+        title: "Triples shaker output",
+        requirement: () => proteinShakers >= 25,
+        bought: false,
+        effect(){
+            proteinShakerMult *= 3;
+        }
+    },
+    {
+        id: "factoriesTier1",
+        name: "Child labor",
+        cost: 5500,
+        title: "cheap workforce(2X factory speed)",
+        requirement: () => factories >= 1,
+        bought: false,
+        effect(){
+            factorieMult *= 2;
         }
     }
 ]
@@ -116,7 +179,15 @@ function renderBuildings(){
         let btn = document.createElement("button");
         let cost = building.getCost();
         btn.className = "buildingBtn";
-        let count = building.id === "riceBots" ? riceBots : proteinShakers;
+        let count;
+        if(building.id === "riceBots"){
+            count = riceBots;
+        }else if(building.id === "proteinShaker3000"){
+            count = proteinShakers
+        }else if(building.id === "factories"){
+            count = factories
+        }
+        
 
         btn.innerHTML = `
         <div class="buildingTitle">${building.name} (${count})</div>
@@ -149,7 +220,7 @@ function renderUpgrades(){
             btn.className = "upgradeBtn";
 
             btn.innerHTML = `
-    <div class="upgradeTitle">${upgrade.name}</div>
+    <div class="upgradeTitle" title="${upgrade.title}">${upgrade.name}</div>
     <div class="upgradeCost">🍗${upgrade.cost}</div>
 `;
 
@@ -173,7 +244,7 @@ friedChickenBtn.onclick = function(){
 }
 setInterval(function(){
 
-    let production = (riceBots * ricebotMult) + (proteinShakers * proteinShakerMult);
+    let production = (riceBots * ricebotMult) + (proteinShakers * proteinShakerMult) + (factories * factorieMult);
 
     protein += production;
 
@@ -206,42 +277,50 @@ function loadGame(){
 
     if(savedGame){
         let saveData = JSON.parse(savedGame);
-    }
         protein = saveData.protein || 0;
         riceBots = saveData.riceBots || 0;
         ricebotCost = saveData.ricebotCost || 15;
         amountPerClick = saveData.amountPerClick || 1;
         ricebotMult = saveData.ricebotMult || 0.1;
+    }
         if(saveData.upgrades){
     upgrades = saveData.upgrades;
     proteinShakers = saveData.proteinShakers || 0;
     proteinShakerMult = saveData.proteinShakerMult || 1;
         upgrades.forEach(upgrade => {
-        if(upgrade.id === "doubleClick"){
-            upgrade.effect = function(){
-                amountPerClick *= 2;
-            }
-        }
-        if(upgrade.id === "AA battery"){
-            upgrade.effect = function(){
-                ricebotMult *= 2;
-            }
-        }
-        if(upgrade.id === "quadrupleClick"){
-            upgrade.effect = function(){
-                amountPerClick *= 2;
-            }
-        }
-        if(upgrade.id === "AAAA battery"){
-            upgrade.effect = function(){
-                ricebotMult *= 2;
-            }
-        }
-        if(upgrade.id === "2X Protein powder"){
-    upgrade.effect = function(){
-        proteinShakerMult *= 2;
+      upgrades.forEach(upgrade => {
+
+    if(upgrade.id === "clickTier1"){
+        upgrade.effect = () => amountPerClick *= 2;
     }
-}
+    if(upgrade.id === "clickTier2"){
+        upgrade.effect = () => amountPerClick *= 2;
+    }
+    if(upgrade.id === "clickTier3"){
+        upgrade.effect = () => amountPerClick *= 2;
+    }
+
+    if(upgrade.id === "riceTier1"){
+        upgrade.effect = () => ricebotMult *= 2;
+    }
+    if(upgrade.id === "riceTier2"){
+        upgrade.effect = () => ricebotMult *= 2;
+    }
+    if(upgrade.id === "riceTier3"){
+        upgrade.effect = () => ricebotMult *= 3;
+    }
+
+    if(upgrade.id === "shakerTier1"){
+        upgrade.effect = () => proteinShakerMult *= 2;
+    }
+    if(upgrade.id === "shakerTier2"){
+        upgrade.effect = () => proteinShakerMult *= 2;
+    }
+    if(upgrade.id === "shakerTier3"){
+        upgrade.effect = () => proteinShakerMult *= 3;
+    }
+
+});
     });
         let now = Date.now();
 let timePassed = (now - saveData.lastPlayed) / 1000;
